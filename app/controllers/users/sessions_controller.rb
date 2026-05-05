96036f7d77a6758575192af508f221631e115d12
caiotarso <caiotarso7@gmail.com>
2026-05-05 18:06:27 -0300

    feat: add text response to logout

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,12 +6,15 @@ class Users::SessionsController < Devise::SessionsController
   def respond_with(resource, _opts = {})
     render json: {
       user: user_payload(resource),
+      token: request.env["warden-jwt_auth.token"],
       message: "Logged in successfully."
     }, status: :ok
   end
 
   def respond_to_on_destroy(*)
-    head :no_content
+    render json: {
+      message: "Logged out successfully."
+    }, status: :ok
   end
 
   def user_payload(user)
