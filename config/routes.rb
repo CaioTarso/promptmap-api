a0216f3041a2083befbe100a6d6684b93273cb81
caiotarso <caiotarso7@gmail.com>
2026-05-05 18:26:29 -0300

    feat: implement "me" route to return logged user info

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,8 @@ Rails.application.routes.draw do
              },
              defaults: { format: :json }
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "me", to: "users/me#show"
+
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
