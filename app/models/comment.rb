d46da5361bef4fbefec773341d6bcb64eeab92de
caiotarso <caiotarso7@gmail.com>
2026-06-05 21:21:31 -0300

    feat: Add comments validation - cannot reply a reply - can only reference actual prompt

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,24 @@ class Comment < ApplicationRecord
   has_many :replies, class_name: "Comment", foreign_key: "parent_id", dependent: :destroy
   has_many :comment_likes, dependent: :destroy
   has_many :liked_by, through: :comment_likes, source: :user
+  validate :parent_must_be_root_comment
+  validate :parent_belongs_to_same_prompt
+
+
+  def parent_must_be_root_comment
+    return if parent.nil?
+
+    if parent.parent_id.present?
+      errors.add(:parent_id, "cannot reply to a reply")
+    end
+  end
+
+
+  def parent_belongs_to_same_prompt
+    return if parent.nil?
+
+    if parent.prompt_id != prompt_id
+      errors.add(:parent_id, "must belong to the same prompt")
+    end
+  end
 end
